6f42ffaee288de7822c08ad0ec2509fa5252c48d
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-09 21:45:57 -0700

    Update installpilptpsettings.sh

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -1,31 +1,8 @@
-unzip /home/pi/lptpsettings/lptpsettings.zip
+unzip /home/pi/lptpsettings/lptpsettins.zip
 
 mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.desktop
 
 sudo rm -d -r /home/pi/lptpsettings
-
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
-echo "                                                                                                                                                              "
 
 echo "                                                                                                                                                              "
 echo "                                                                                                                                                              "
