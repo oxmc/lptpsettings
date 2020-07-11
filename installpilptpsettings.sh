faa3c0f7bbfc7d1456105d5ae3db89f32d73bb47
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-11 22:52:19 +0100

    Updated installpilptpsettings.sh

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -2,7 +2,8 @@ mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.deskt
 
 unzip /home/pi/lptpsettings/lptpsettins.zip
 
+mv /home/pi/lptpsettings/home/pi/.lptpsettings /home/pi/.lptpsettings
+
 rm -d -r /home/pi/lptpsettings
 
 echo "Succesfuly installed lptpsettings to this device!"
-
