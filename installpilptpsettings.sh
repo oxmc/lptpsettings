5cbeaedc93627f0c6e613da621881f34b7bbb1f2
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-14 03:29:13 +0100

    Updated installpilptpsettings.sh

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -1,5 +1,9 @@
 sudo mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.desktop
 
+sudo mv /home/pi/lptpsettings/uninstalllptpsettings.sh /home/pi/uninstalllptpsettings.sh
+
+chmod +x /home/pi/uninstalllptpsettings.sh
+
 unzip /home/pi/lptpsettings/lptpsettings.zip
 
 mv /home/pi/lptpsettings/home/pi/.lptpsettings /home/pi/.lptpsettings
