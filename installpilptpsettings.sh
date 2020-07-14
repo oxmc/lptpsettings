b893aa285f33775b5fc123ceb96f66e292319fed
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-14 03:40:43 +0100

    Updated uninstallpilptpsettings.sh
    fixed a typo in the code.

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -1,6 +1,6 @@
 sudo mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.desktop
 
-sudo mv /home/pi/lptpsettings/uninstalllptpsettings.sh /home/pi/uninstalllptpsettings.sh
+sudo mv /home/pi/lptpsettings/uninstalllptpsettings.sh /home/pi/uninstallpilptpsettings.sh
 
 chmod +x /home/pi/uninstalllptpsettings.sh
 
