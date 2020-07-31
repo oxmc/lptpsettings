846050b2b3ef8c076e919e7af04a4e5367afc79a
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-31 15:54:38 +0100

    Updated installpilptpsettings.sh
    Fixed progress bar not working.

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -1,29 +1,29 @@
-zenity --progress --text="Starting instilation" --progres="0"
+zenity --progress --text="Starting instilation" --progress="0"
 
 sleep 1
 
-zenity --progress --text="Moving files" --progres="5"
+zenity --progress --text="Moving files" --progress="5"
 
 sudo mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.desktop
 
 sudo mv /home/pi/lptpsettings/uninstallpilptpsettings.sh /home/pi/uninstallpilptpsettings.sh
 
-zenity --progress --text="Making files executible" --progres="10"
+zenity --progress --text="Making files executible" --progress="10"
 
 chmod +x /home/pi/uninstalllptpsettings.sh
 
-zenity --progress --text="Unzipping files" --progres="20"
+zenity --progress --text="Unzipping files" --progress="20"
 
 unzip /home/pi/lptpsettings/lptpsettings.zip
 
-zenity --progress --text="Files unziped, now moving into proper directorys" --progres="40"
+zenity --progress --text="Files unziped, now moving into proper directorys" --progress="40"
 
 mv /home/pi/lptpsettings/home/pi/.lptpsettings /home/pi/.lptpsettings
 
-zenity --progress --text="Removing unused files" --progres="60"
+zenity --progress --text="Removing unused files" --progress="60"
 
 rm -d -r /home/pi/lptpsettings
 
-zenity --progress --text="Instilation completed!" --progres="100"
+zenity --progress --text="Instilation completed!" --progress="100"
 
 zenity --progress --text="Succesfuly installed lptpsettings to this device!" --progress="100" --auto-close
