d420b11cf5a20402d0f278ce18cc188733e3b07d
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-31 15:49:13 +0100

    Updated installpilptpsettings.sh
    Added progress bars so the user knows what's happening.

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -1,13 +1,29 @@
+zenity --progress --text="Starting instilation" --progres="0"
+
+sleep 1
+
+zenity --progress --text="Moving files" --progres="5"
+
 sudo mv /home/pi/lptpsettings/settings.desktop /usr/share/applications/settings.desktop
 
 sudo mv /home/pi/lptpsettings/uninstallpilptpsettings.sh /home/pi/uninstallpilptpsettings.sh
 
+zenity --progress --text="Making files executible" --progres="10"
+
 chmod +x /home/pi/uninstalllptpsettings.sh
+
+zenity --progress --text="Unzipping files" --progres="20"
 
 unzip /home/pi/lptpsettings/lptpsettings.zip
 
+zenity --progress --text="Files unziped, now moving into proper directorys" --progres="40"
+
 mv /home/pi/lptpsettings/home/pi/.lptpsettings /home/pi/.lptpsettings
+
+zenity --progress --text="Removing unused files" --progres="60"
 
 rm -d -r /home/pi/lptpsettings
 
-echo "Succesfuly installed lptpsettings to this device!"
+zenity --progress --text="Instilation completed!" --progres="100"
+
+zenity --progress --text="Succesfuly installed lptpsettings to this device!" --progress="100" --auto-close
