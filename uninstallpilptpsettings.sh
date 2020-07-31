8c8023873aaec570661923855f349bd5fd8a2268
oxmc <67136658+oxmc@users.noreply.github.com>
2020-07-31 15:56:35 +0100

    Updated uninstallpilptpsettings.sh
    Fixed uninstallpilptpsettings.sh from not deleting files and directorys from the computer.

--- uninstallpilptpsettings.sh
+++ uninstallpilptpsettings.sh
@@ -2,3 +2,4 @@ sudo rm -r /home/pi/.lptpsettings
 sudo rm -r /usr/share/applications/Laptop.desktop
 sudo rm -r /home/pi/uninstalllptpsettings.sh
 echo "Uninstalled lptpsettings from this device!"
+
