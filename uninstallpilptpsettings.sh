caa752452f43ead3046caf9258c581be15ae7e0c
oxmc <67136658+oxmc@users.noreply.github.com>
2020-08-01 02:22:53 +0100

    Update uninstallpilptpsettings.sh

--- uninstallpilptpsettings.sh
+++ uninstallpilptpsettings.sh
@@ -1,5 +1,6 @@
 sudo rm -r /home/pi/.lptpsettings
 sudo rm -r /usr/share/applications/Laptop.desktop
-sudo rm -r /home/pi/uninstalllptpsettings.sh
 echo "Uninstalled lptpsettings from this device!"
+sleep 1
+sudo rm -r /home/pi/uninstallpilptpsettings.sh
 
