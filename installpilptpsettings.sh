6a8e64871f315d36f0f3e34cccea834765cac440
oxmc <67136658+oxmc@users.noreply.github.com>
2020-11-17 07:31:48 -0800

    Update installpilptpsettings.sh

--- installpilptpsettings.sh
+++ installpilptpsettings.sh
@@ -22,7 +22,7 @@ echo "# Getting rid of useles files" ; sleep 1
 echo "85" ; sleep 1
 rm -d -r /home/pi/lptpsettings
 echo "90" ; sleep 1
-rm -d -r /home/pi/home/* ; sleep 1
+rm -d -r /home/pi/home ; sleep 1
 echo "100"
 echo "# Succesfully installed lptpsettings to this device!" ; sleep 1
 ) |
